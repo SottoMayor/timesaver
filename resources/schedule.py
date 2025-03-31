0614415fad86cbbc20bc2e18bb3ad0dfc8d724b4
SottoMayor <ddsmmf@gmail.com>
2025-03-31 18:41:38 -0400

    Saving the schedule

--- resources/schedule.py
+++ resources/schedule.py
@@ -1,6 +1,11 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask_smorest import Blueprint
 from flask.views import MethodView
+from flask import request
+from marshmallow import ValidationError
+from schemas.schedule_schema import ScheduleSchema
+from database.connection import db
+from models.schedule import ScheduleModel
 
 blp = Blueprint('Schedules', __name__, url_prefix="/schedules", description='Blueprint para agendamentos.')
 
@@ -10,7 +15,23 @@ class Schedules(MethodView):
         return render_template("index.html")
     
     def post(self):
-        return 'POST SCHEDULE'
+        schema = ScheduleSchema()
+        try:
+            data = schema.load(request.form)
+        except ValidationError as err:
+            return {"errors": err.messages}, 400
+
+        schedule = ScheduleModel(
+        client=data['cliente'],
+        service=data['servico'],
+        schedule_date=data['data'],
+        schedule_time=data['horario']
+        )
+
+        db.session.add(schedule)
+        db.session.commit()
+
+        return redirect(url_for('Schedules.Schedules'))
     
     def patch(self):
         return 'PATCH SCHEDULE'
